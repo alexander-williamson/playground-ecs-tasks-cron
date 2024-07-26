b685056a34a9a4a05129f77e2cbaae40ad4dce38
Alex Williamson <alex.williamson@bbc.com>
2024-07-26 16:34:24 +0100

    Add cron job

--- stack/main.tf
+++ stack/main.tf
@@ -1,7 +1,18 @@
 locals {
-  image_name = "test-cron-runnner"
+  image_name = "test-cron-runner"
   stack_name = "test-playground-ecs-tasks-cron"
+  schedule_expression = "cron(0 * * * ? *)"
 }
+
+# lookups
+
+data "aws_subnets" "public" {
+  tags = {
+    Tier = "Public"
+  }
+}
+
+# ecr
 
 resource "aws_ecr_repository" "foo" {
   name                 = local.image_name
@@ -11,6 +22,8 @@ resource "aws_ecr_repository" "foo" {
     scan_on_push = true
   }
 }
+
+# task definitions
 
 resource "aws_cloudwatch_log_group" "logs" {
   name = "/ecs/${local.stack_name}/${local.image_name}"
@@ -50,4 +63,33 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       },
     }
   ])
+}
+
+# cron
+
+resource "aws_cloudwatch_event_rule" "scheduled_task" {
+  name = "${local.stack_name}-cron"
+  description = "Runs fargate task ${local.stack_name}: ${local.schedule_expression}"
+  schedule_expression = local.schedule_expression
+}
+
+resource "aws_cloudwatch_event_target" "scheduled_task" {
+  rule = aws_cloudwatch_event_rule.scheduled_task.name
+  arn = aws_ecs_cluster.playground_ecs_cluster.arn
+  role_arn = aws_iam_role.ecs_task_execution_role.arn
+  input = "{}"
+
+  ecs_target {
+    task_count = 1
+    task_definition_arn = aws_ecs_task_definition.ecs_task_definition.arn
+    launch_type = "FARGATE"
+    platform_version = "LATEST"
+
+    network_configuration {
+      assign_public_ip = true
+      subnets = data.aws_subnets.public.ids
+    }
+  }
+
+  
 }
